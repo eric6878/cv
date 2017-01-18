dc6d62a9708459c4423feae10f80a8425a0bc3f6
éric coudert <eric.coudert@lepoles.com>
2017-01-18 17:09:52 +0100

    sauvegarde du dossier sitecv avec le fichier css bootstrap intégré

--- admin/index.php
+++ admin/index.php
@@ -1,40 +1,6 @@
 
 <?php require '../connexion/connexion.php' ?>
 
-
-<?php
-
-// pour se connecter :
-
-session_start();
-
-if(isset($_SESSION['connexion']) && $_SESSION['connexion'] == 'Vous êtes connecté !'){
-  $id_utilisateur = $_SESSION['id_utilisateur'];
-  $prenom = $_SESSION['prenom'];
-  $nom = $_SESSION['nom'];
-}
-else{
-  header('location: authentification.php');
-}
-
-
-// pour se deconnecter : 
-
-if(isset($_GET['deconnexion'])){
-  $_SESSION['connexion'] = '';
-  $_SESSION['id_utilisateur'] = '';
-  $_SESSION['prenom'] = '';
-  $_SESSION['nom'] = '';
-
-  unset($_SESSION['connexion']);
-  session_destroy();
-  header('location: ../index.php');
-}
-
-?>
-
-
- 
 <?php 
 
 session_start();
---
+++ index2.php
@@ -0,0 +1,41 @@
+<?php require 'connexion/connexion.php'; ?>
+
+<?php
+
+// pour se connecter :
+session_start();
+
+if(isset($_SESSION['connexion']) && $_SESSION['connexion'] == 'Vous êtes connecté !'){
+  $id_utilisateur = $_SESSION['id_utilisateur'];
+  $prenom = $_SESSION['prenom'];
+  $nom = $_SESSION['nom'];
+}
+else{
+  header('location: authentification.php');
+}
+
+
+// pour se deconnecter : 
+
+if(isset($_GET['deconnexion'])){
+  $_SESSION['connexion'] = '';
+  $_SESSION['id_utilisateur'] = '';
+  $_SESSION['prenom'] = '';
+  $_SESSION['nom'] = '';
+
+  unset($_SESSION['connexion']);
+  session_destroy();
+  header('location: ../index.php');
+}
+
+?>
+
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+	<title>Site CV éric coudert</title>
+</head>
+<body>
+<h1>Hello world !!!</h1>
+</body>
+</html>
