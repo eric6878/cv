f13d1c5b393bc7f8a7e367d2f2e2baa31ff7c6e4
éric coudert <eric.coudert@lepoles.com>
2017-01-24 16:41:16 +0100

    Ajout du dossier parallax + mise à jour du site.

--- index.php
+++ index.php
@@ -49,7 +49,7 @@
     <!--[if lt IE 9]>
         <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
         <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
-    <![endif]--> <section>  
+    <![endif]-->  
 
 </head>
 
@@ -58,81 +58,119 @@
     <!-- Navigation -->
     <nav class="navbar navbar-custom navbar-fixed-top" role="navigation">
         <div class="container-fluid">
-            <div class="navbar-header">
-                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-main-collapse">
-                <i class="fa fa-bars"></i>
-                </button>
-                <a class="navbar-brand page-scroll" data-toggle="collapse" href="#btnConnexionAdmin">
-                    <i class="fa fa-play-circle"></i>
-                    <span class="light">Admin</span>
-                    <div >
-                        <a href="admin/authentification.php" >Connexion</a>
-                    </div>
+            <div class="col-lg-1">
+                <a id="btnAdmin" class="btn btn-primary" data-toggle="collapse" href="#linkConnexionAdmin" aria-expanded="false" aria-controls="collapseExample">
+                Admin
                 </a>
+                <div class="collapse" id="linkConnexionAdmin">
+                    <a href="admin/authentification.php">Connexion</a>
+                </div>
             </div>
 
-            <!-- Collect the nav links, forms, and other content for toggling -->
-            <div class="collapse navbar-collapse navbar-right navbar-main-collapse">
-                <ul class="nav navbar-nav">
-                    <!-- Hidden li included to remove active class from about link when scrolled up past about section -->
-                    <li class="hidden">
-                        <a href="#page-top"></a>
-                    </li>
-                    <li>
-                        <a class="page-scroll active" href="#">Home</a>
-                    </li>
-                    <li>
-                        <a class="page-scroll" href="#">Inspirations</a>
-                    </li>
-                    <li>
-                        <a class="page-scroll" href="#about">À propos</a>
-                    </li>
-                    <li>
-                        <a class="page-scroll" href="#">Portfolio</a>
-                    </li>
-                    <li>
-                        <a class="page-scroll" href="#downloadCv">CV à télécharger</a>
-                    </li>
-                    <li>
-                        <a class="page-scroll" href="#contact">Contact</a>
-                    </li>
-                    <li>
-                        <form method="POST" action="#" class="form-inline">
-                        <input type="search" value="search..." class="form-control" />
-                        <button type="submit" class="btn btn-primary">Submit</button>
-                        </form> 
-                    </li>
+            <div class="col-lg-offset-2 col-lg-8">
+                <!-- Collect the nav links, forms, and other content for toggling -->
+                <div id="navCenter" class="collapse navbar-collapse navbar-main-collapse">
+                    <ul class="nav navbar-nav">
+                        <!-- Hidden li included to remove active class from about link when scrolled up past about section -->
+                        <li class="hidden">
+                            <a href="#page-top"></a>
+                        </li>
+                        <li>
+                            <a id="linkHome" class="page-scroll" href="#">Home</a>
+                        </li>
+                        <li>
+                            <a class="page-scroll" href="#">Inspirations</a>
+                        </li>
+                        <li>
+                            <a class="page-scroll" href="#about">À propos</a>
+                        </li>
+                        <li>
+                            <a class="page-scroll" href="#">Portfolio</a>
+                        </li>
+                        <li>
+                            <a class="page-scroll" href="#downloadCv">CV à télécharger</a>
+                        </li>
+                        <li>
+                            <a class="page-scroll" href="#contact">Contact</a>
+                        </li>
+                    </ul> 
+                </div>
+            </div><!-- /.navbar-collapse -->
 
-                </ul> 
-            </div>
-            <!-- /.navbar-collapse -->
+            <!-- <div class="input-group col-lg-2">
+              <input type="text" class="form-control" placeholder="Search for...">
+              <span class="input-group-btn">
+                <button class="btn btn-secondary" type="button">Go!</button>
+              </span>
+            </div> -->
         </div>
         <!-- /.container -->
     </nav>
 
     <!-- Intro Header -->
-    <header class="intro">
+    <header class="intro" class="parallax-window" data-parallax="scroll" data-image-src="img/imgPetronasTowers.jpg">
     <?php $sql = $pdoCV -> query("SELECT * FROM utilisateurs");
           $resultat = $sql -> fetch(); ?>
         <div class="intro-body">
             <div class="container">
                 <div class="row">
-                    <div class="col-md-8 col-md-offset-2">
-                        <h1 class="brand-heading">ID web<br /></h1>
-                        <address><a href="#"><?= $resultat['email']; ?></a></address>
-                        <h2 class="intro-text">
-                        <br>
-                        Intégrateur
-                        <br />
-                        développeur
-                        <br />
-                        web
-                        <br>
-                        junior
-                        </h2>
-                        <a href="#cv" class="btn btn-circle page-scroll">
-                            <i class="fa fa-angle-double-down animated"></i>
-                        </a>
+                    <div class="col-md-8 col-md-offset-2"> 
+                        <div id="titleContainer" class="row">
+                            <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+                                <h1>
+                                    <?= $resultat['prenom'] . ' ' . $resultat['nom']; ?>
+                                </h1>
+                                <div id="title">
+                                    <div class="titleContent1">
+                                      <div class="black"><p>I</p></div>
+                                      <div class="black">N</div>
+                                      <div class="black">T</div>
+                                      <div class="black">E</div>
+                                      <div class="black">G</div>
+                                      <div class="black">R</div>
+                                      <div class="black">A</div>
+                                      <div class="black">T</div>
+                                      <div class="black">E</div>
+                                      <div class="black">U</div>
+                                      <div class="black">R</div>
+                                    </div>
+                                    <div class="titleContent2">
+                                        <div id="black" class="phantom"><p>D</p></div>
+                                        <div class="black">E</div>
+                                        <div class="black">V</div>
+                                        <div class="black">E</div>
+                                        <div class="black">L</div>
+                                        <div class="black">O</div>
+                                        <div class="black">P</div>
+                                        <div class="black">P</div>
+                                        <div class="black">E</div>
+                                        <div class="black">U</div>
+                                        <div class="black">R</div>
+                                    </div>
+                                    <div class="titleContent2">
+                                        <div id="white" class="phantom"></div>
+                                        <div class="blackOut"></div>
+                                        <div class="blackOut"></div>
+                                        <div class="blackOut"></div>
+                                        <div class="black">W</div>
+                                        <div class="black">E</div>
+                                        <div class="black">B</div>
+                                        <div class="blackOut"></div>
+                                        <div class="blackOut"></div>
+                                        <div class="blackOut"></div>
+                                        <div class="blackOut"></div>
+                                    </div>
+                                </div>
+                                
+                                <div class="junior">Junior</div>
+                                 
+                                <div>
+                                    <a href="#cv" class="btn btn-circle page-scroll">
+                                        <i id="btnFleche" class="fa fa-angle-double-down animated"></i>
+                                    </a>
+                                </div>
+                            </div>
+                        </div>
                     </div>
                 </div>
             </div>
@@ -140,62 +178,42 @@
     </header>
 
     <!-- Download Section -->
-    <section id="cv" class="content-section text-center">
-        <div class="download-section">
+    <section id="cv" class="content-section text-center parallax-window" data-parallax="scroll" data-image-src="img/imgPetronasTowers.jpg">
+        <div class="download-section"> 
+        <h2>Compétences numériques</h2>
             <div class="container">
                 <div class="col-lg-8 col-lg-offset-2">
-                    <h2>Curriculum Vitae</h2>
-                    
-                    <br />
-
+                
                     <h3>Formation</h3>
 
-                    <?php 
-                        $i = 0;
-                        while($i < count($formation)){
-                            if($i > 0){
-                                echo ' / ';
+                    <p>2016- 2017<p>
+
+                    <p>
+                        <?php 
+                            $i = 0;
+                            while($i < count($formation)){
+                                if($i > 0){
+                                    echo ' / ';
+                                }
+                                echo $formation[$i]['titre_formation'];
+                                $i++;
                             }
-                            echo $formation[$i]['titre_formation'];
-                            $i++;
-                        }
-                    ?>
+                        ?>
+                    </p>
 
-                    <br /><br /><br />
+                    <h3>Intégration &amp; développement</h3>
 
-                    <h3>Compétences numériques</h3>
+                    <p>HTML5 / CSS3</p>
+                    <p>JavaScript / JQuery</p>
+                    <p>MySQL / PHP5</p>
+                    <p>Ligne de commande Git / Linux</p>
 
-                    <?php 
-                        $i = 0;
-                        while($i < count($competence)){
-                            if($i > 0){
-                                echo ' / ';
-                            }
-                            echo $competence[$i]['competence'];
-                            $i++;
-                        }
-                    ?>
+                    <h3>Projets</h3>
 
-                    <br /><br /><br />
-
-                    <h3>Expériences</h3>
-
-                    <p>Intrégration d'un site CV dynamique<br />
-                    <a href="#">exemple site 1</a></p>
-                    <p>Intrégration d'un site web type Worpress<br />
-                    <a href="#">exemple site 2</a></p>
-
-                    <?php 
-                        /*$i = 0;
-                        while($i < count($experience)){
-                            if($i > 0){
-                                echo ' / ';
-                            }
-                            echo $experience[$i]['titre_xp'];
-                            $i++;
-                        }*/
-                    ?>
-                    <br />
+                    <p>Intrégration d'un site CV dynamique</p>
+                    <p><a href="#">exemple site 1</a></p>
+                    <p>Intrégration d'un site web type Worpress</p>
+                    <p><a href="#">exemple site 2</a></p>
                     
                     <a href="http://startbootstrap.com/template-overviews/grayscale/" class="btn btn-default btn-lg">CV à télécharger</a>
                 </div>
@@ -204,21 +222,23 @@
     </section>
 
      <!-- About Section -->
-    <section class="container content-section text-center">
+    <section id="about" class="container content-section text-center">
         <div class="row">
-            <div class="col-lg-8 col-lg-offset-2">
+            <div id="about" class="col-lg-8 col-lg-offset-2">
                 
-                <h2>À mon propos</h2>
-                
-                <p>Agé de 38 ans, autonome, polyvalent et curieux,<br />
-                j’ai démarré cette activité comme un hobby il y a 1 an,<br />
-                puis ai décidé d'en faire mon métier par passion.</p>
-                    <!--  <p>En effet, après plusieurs années passées dans les domaines de la restauration, de la livraison et du service, il m'a parut nécessaire de prendre un virage professionnel dans une ère numérique qui me semble incontournable aujourd'hui.
-                     </p> -->
-                <p>Concepteur de sites de A à Z ou bien de "frameworks",<br />
-                je fais en sorte que mon code soit propre et optimisé le plus souvent possible.</p>
+                <h2>À propos de moi...</h2>
+            
+                <p>Agé de 38 ans, autonome, polyvalent et curieux, j’ai démarré cette activité comme un hobby en 2015,</p>
+                <p>puis ai décidé d'en faire mon métier par passion.</p>
 
-                <p>Passionné du web,<br /> je mets à contribution mon sens créatif et artistique à votre service,<br /> dans un domaine qui m'enthousiasme au plus haut point,<br /> convaincu que <a href="#">l' avenir s'écrit en lignes de code</a>..."</p>
+                <p>Actuellement en formation au métier d'intégrateur développeur,</p>
+                <p>je conçois des sites web from "scratch" ou bien de type "frameworks / CMS"</p>
+                <p>et fais en sorte que mon code soit propre et optimisé le plus souvent possible.</p>
+
+                <p>Passionné du web,</p>
+                <P>je mets à contribution mon sens créatif et artistique à votre service,</p>
+                <p>dans un domaine qui m'enthousiasme au plus haut point,</p>
+                <p>convaincu que <a href="#">l' avenir s'écrit en lignes de code</a>..."</p>
            
             </div>
         </div>
@@ -265,6 +285,10 @@
             <p>Copyright&copy; Site CV éric coudert 2017</p>           
         </div>
     </footer>
+
+    <!-- Parallax -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
+    <script src="parallax/parallax.js"></script>
 
     <!-- jQuery -->
     <script src="cssfrontbootstrap/vendor/jquery/jquery.js"></script>
